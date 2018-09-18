6f7ae38ea89a3f990637e5f4915109411e40cdd0
Gunipati <893108@corpaa.aa.com>
2018-09-18 14:55:05 -0500

    Iteration3 - Test controllerconstants slack

--- apt-services/src/main/java/com/aa/apt/constants/ControllerConstants.java
+++ apt-services/src/main/java/com/aa/apt/constants/ControllerConstants.java
@@ -1,9 +1,19 @@
 package com.aa.apt.constants;
 
-public interface ControllerConstants {
+public final class ControllerConstants {
 	
-	static final String ROOT_API="/api/";
-	static final String PINGTEST="hello";
-	static final String PCODESEARCH="search/{pcode}";
+	private ControllerConstants() {
+	
+	}
+	
+	public static final String ROOT_API="/api/";
+	public static final String PINGSPRINGBOOT="hello";
+	public static final String PCODESEARCH="search/{pcode}";
+	public static final String PCODESEARCHFORAR5="search/ar5/{pcode}";
+	public static final String PCODESEARCHFORACS="search/acs/{pcode}";
 
+	
+	// JSON Requests
+	
+	public static final String VEN_PROMOSEARCH_JSON_REQ="LoyaltyMasterPromotionService_PartialPromo_RequestTemplate.json";
 }
