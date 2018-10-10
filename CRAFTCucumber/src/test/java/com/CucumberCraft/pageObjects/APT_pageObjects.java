39f2f2fc601f188efe039bc0b4f337c3425220b8
00893377 <Ajit.Yadav@aa.com>
2018-10-10 21:33:08 +0530

    Revert "testing framework"
    This reverts commit 3ba6ade81b1a70dd3c488ae85771e919e8c963cf.

--- CRAFTCucumber/src/test/java/com/CucumberCraft/pageObjects/APT_pageObjects.java
+++
@@ -1,5 +0,0 @@
-package com.CucumberCraft.pageObjects;
-
-public class APT_pageObjects {
-	public static final String SeachboxLabel ="//div/input[@id='ptextcode']/ancestor::div/label[' Promo / PST / TA Code ']" ;
-}
