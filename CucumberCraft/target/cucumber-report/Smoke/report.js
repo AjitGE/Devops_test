37e40e28a579eb2a2d447455817826803c34ddde
Ajit <ajit.yadav@aa.com>
2018-10-23 21:04:04 +0530

    added nexus repo in pom.xml

--- CucumberCraft/target/cucumber-report/Smoke/report.js
+++ CucumberCraft/target/cucumber-report/Smoke/report.js
@@ -7,7 +7,7 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.before({
-  "duration": 23595963349,
+  "duration": 7953983867,
   "status": "passed"
 });
 formatter.scenario({
@@ -30,7 +30,7 @@ formatter.scenario({
 });
 formatter.step({
   "line": 5,
-  "name": "user loads the data from \"excel\" located at \"src\\test\\resources\\Fixture\\Promo_DB_Data.xlsx\"",
+  "name": "user loads the data from \"excel\" located at \"src/test/resources/Fixture/Promo_DB_Data.xlsx\"",
   "keyword": "Given "
 });
 formatter.step({
@@ -65,14 +65,14 @@ formatter.match({
       "offset": 26
     },
     {
-      "val": "src\\test\\resources\\Fixture\\Promo_DB_Data.xlsx",
+      "val": "src/test/resources/Fixture/Promo_DB_Data.xlsx",
       "offset": 45
     }
   ],
   "location": "Browser_Excel_StepDef.user_loads_the_data_from_located_at(String,String)"
 });
 formatter.result({
-  "duration": 2072274336,
+  "duration": 2488944184,
   "status": "passed"
 });
 formatter.match({
@@ -84,10 +84,10 @@ formatter.match({
   ],
   "location": "Browser_Excel_StepDef.user_launches_browser_with_url(String)"
 });
+formatter.embedding("image/png", "embedded0.png");
 formatter.result({
-  "duration": 27619986,
-  "error_message": "java.lang.ExceptionInInitializerError\r\n\tat com.CucumberCraft.Screenshot.ScreenshotTaker.\u003cclinit\u003e(ScreenshotTaker.java:19)\r\n\tat com.CucumberCraft.stepDefinitions.Browser_Excel_StepDef.user_launches_browser_with_url(Browser_Excel_StepDef.java:28)\r\n\tat ✽.Then user launches browser with url \"#TestUrl\"(UserStory_US869555.feature:6)\r\nCaused by: java.lang.NullPointerException\r\n\tat com.CucumberCraft.supportLibraries.DriverManager.setWebDriver(DriverManager.java:89)\r\n\tat com.CucumberCraft.supportLibraries.DriverManager.getWebDriver(DriverManager.java:47)\r\n\tat com.CucumberCraft.Screenshot.EventHandler.\u003cclinit\u003e(EventHandler.java:16)\r\n\tat com.CucumberCraft.Screenshot.ScreenshotTaker.\u003cclinit\u003e(ScreenshotTaker.java:19)\r\n\tat com.CucumberCraft.stepDefinitions.Browser_Excel_StepDef.user_launches_browser_with_url(Browser_Excel_StepDef.java:28)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:40)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:34)\r\n\tat cucumber.runtime.java.JavaStepDefinition.execute(JavaStepDefinition.java:38)\r\n\tat cucumber.runtime.StepDefinitionMatch.runStep(StepDefinitionMatch.java:37)\r\n\tat cucumber.runtime.Runtime.runStep(Runtime.java:300)\r\n\tat cucumber.runtime.model.StepContainer.runStep(StepContainer.java:44)\r\n\tat cucumber.runtime.model.StepContainer.runSteps(StepContainer.java:39)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:44)\r\n\tat cucumber.runtime.model.CucumberFeature.run(CucumberFeature.java:165)\r\n\tat cucumber.api.testng.TestNGCucumberRunner.runCucumber(TestNGCucumberRunner.java:63)\r\n\tat cucumber.api.testng.AbstractTestNGCucumberTests.feature(AbstractTestNGCucumberTests.java:21)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat org.testng.internal.MethodInvocationHelper.invokeMethod(MethodInvocationHelper.java:108)\r\n\tat org.testng.internal.Invoker.invokeMethod(Invoker.java:661)\r\n\tat org.testng.internal.Invoker.invokeTestMethod(Invoker.java:869)\r\n\tat org.testng.internal.Invoker.invokeTestMethods(Invoker.java:1193)\r\n\tat org.testng.internal.TestMethodWorker.invokeTestMethods(TestMethodWorker.java:126)\r\n\tat org.testng.internal.TestMethodWorker.run(TestMethodWorker.java:109)\r\n\tat org.testng.TestRunner.privateRun(TestRunner.java:744)\r\n\tat org.testng.TestRunner.run(TestRunner.java:602)\r\n\tat org.testng.SuiteRunner.runTest(SuiteRunner.java:380)\r\n\tat org.testng.SuiteRunner.access$000(SuiteRunner.java:39)\r\n\tat org.testng.SuiteRunner$SuiteWorker.run(SuiteRunner.java:414)\r\n\tat org.testng.internal.thread.ThreadUtil$1.call(ThreadUtil.java:52)\r\n\tat java.util.concurrent.FutureTask.run(FutureTask.java:266)\r\n\tat java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)\r\n\tat java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)\r\n\tat java.lang.Thread.run(Thread.java:748)\r\n",
-  "status": "failed"
+  "duration": 14295845175,
+  "status": "passed"
 });
 formatter.match({
   "arguments": [
@@ -102,14 +102,20 @@ formatter.match({
   ],
   "location": "UserStory_US859342.user_enters_the_Promo_code_in_Search_box_named(String,String)"
 });
+formatter.embedding("image/png", "embedded1.png");
+formatter.embedding("image/png", "embedded2.png");
 formatter.result({
-  "status": "skipped"
+  "duration": 1001690141,
+  "status": "passed"
 });
 formatter.match({
   "location": "UserStory_US859342.user_clicks_on_the_search_button()"
 });
+formatter.embedding("image/png", "embedded3.png");
+formatter.embedding("image/png", "embedded4.png");
 formatter.result({
-  "status": "skipped"
+  "duration": 900229447,
+  "status": "passed"
 });
 formatter.match({
   "arguments": [
@@ -120,18 +126,34 @@ formatter.match({
   ],
   "location": "UserStory_US869555.user_checks_that_maximum_promo_box_are_present_in_search_result(int)"
 });
+formatter.embedding("image/png", "embedded5.png");
+formatter.embedding("image/png", "embedded6.png");
+formatter.embedding("image/png", "embedded7.png");
+formatter.embedding("image/png", "embedded8.png");
+formatter.embedding("image/png", "embedded9.png");
+formatter.embedding("image/png", "embedded10.png");
+formatter.embedding("image/png", "embedded11.png");
 formatter.result({
-  "status": "skipped"
+  "duration": 5289054241,
+  "status": "passed"
 });
 formatter.match({
   "location": "UserStory_US869555.user_can_see_page_number_and_next_icon()"
 });
+formatter.embedding("image/png", "embedded12.png");
+formatter.embedding("image/png", "embedded13.png");
+formatter.embedding("image/png", "embedded14.png");
+formatter.embedding("image/png", "embedded15.png");
+formatter.embedding("image/png", "embedded16.png");
+formatter.embedding("image/png", "embedded17.png");
+formatter.embedding("image/png", "embedded18.png");
+formatter.embedding("image/png", "embedded19.png");
 formatter.result({
-  "status": "skipped"
+  "duration": 2581027815,
+  "status": "passed"
 });
 formatter.after({
-  "duration": 766235083,
-  "error_message": "java.lang.NullPointerException\r\n\tat com.CucumberCraft.supportLibraries.DriverManager.setWebDriver(DriverManager.java:89)\r\n\tat com.CucumberCraft.supportLibraries.DriverManager.getWebDriver(DriverManager.java:47)\r\n\tat com.CucumberCraft.stepDefinitions.CukeHooks.embedScreenshot(CukeHooks.java:137)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:40)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:34)\r\n\tat cucumber.runtime.java.JavaHookDefinition.execute(JavaHookDefinition.java:60)\r\n\tat cucumber.runtime.Runtime.runHookIfTagsMatch(Runtime.java:224)\r\n\tat cucumber.runtime.Runtime.runHooks(Runtime.java:212)\r\n\tat cucumber.runtime.Runtime.runAfterHooks(Runtime.java:206)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:46)\r\n\tat cucumber.runtime.model.CucumberFeature.run(CucumberFeature.java:165)\r\n\tat cucumber.api.testng.TestNGCucumberRunner.runCucumber(TestNGCucumberRunner.java:63)\r\n\tat cucumber.api.testng.AbstractTestNGCucumberTests.feature(AbstractTestNGCucumberTests.java:21)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat org.testng.internal.MethodInvocationHelper.invokeMethod(MethodInvocationHelper.java:108)\r\n\tat org.testng.internal.Invoker.invokeMethod(Invoker.java:661)\r\n\tat org.testng.internal.Invoker.invokeTestMethod(Invoker.java:869)\r\n\tat org.testng.internal.Invoker.invokeTestMethods(Invoker.java:1193)\r\n\tat org.testng.internal.TestMethodWorker.invokeTestMethods(TestMethodWorker.java:126)\r\n\tat org.testng.internal.TestMethodWorker.run(TestMethodWorker.java:109)\r\n\tat org.testng.TestRunner.privateRun(TestRunner.java:744)\r\n\tat org.testng.TestRunner.run(TestRunner.java:602)\r\n\tat org.testng.SuiteRunner.runTest(SuiteRunner.java:380)\r\n\tat org.testng.SuiteRunner.access$000(SuiteRunner.java:39)\r\n\tat org.testng.SuiteRunner$SuiteWorker.run(SuiteRunner.java:414)\r\n\tat org.testng.internal.thread.ThreadUtil$1.call(ThreadUtil.java:52)\r\n\tat java.util.concurrent.FutureTask.run(FutureTask.java:266)\r\n\tat java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)\r\n\tat java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)\r\n\tat java.lang.Thread.run(Thread.java:748)\r\n",
-  "status": "failed"
+  "duration": 3676518078,
+  "status": "passed"
 });
 });
