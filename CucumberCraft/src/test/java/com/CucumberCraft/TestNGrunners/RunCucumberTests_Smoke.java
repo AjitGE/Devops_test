9a8d4a674932812cf3f5745ee0de7929d7ccdfb9
AJIT YADAV <Ajit.Yadav@aa.com>
2018-11-02 18:59:13 +0530

    UserStory_US859342

--- CucumberCraft/src/test/java/com/CucumberCraft/TestNGrunners/RunCucumberTests_Smoke.java
+++ CucumberCraft/src/test/java/com/CucumberCraft/TestNGrunners/RunCucumberTests_Smoke.java
@@ -40,7 +40,7 @@ usageReport = true)
 */
 @CucumberOptions(features = "src/test/resources/features", 
 glue = { "com.CucumberCraft.stepDefinitions" }, 
-tags = { "@US859342"},
+tags = { "@UserStory_US859342"},
 monochrome = true,
 plugin = {
     
