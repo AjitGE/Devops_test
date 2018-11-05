92e073b11ef77e9b364746b747619320dfedb10e
Ajit <ajit.yadav@aa.com>
2018-11-05 17:58:57 +0530

    running for whole user story

--- CucumberCraft/src/test/java/com/CucumberCraft/TestNGrunners/RunCucumberTests_Smoke.java
+++ CucumberCraft/src/test/java/com/CucumberCraft/TestNGrunners/RunCucumberTests_Smoke.java
@@ -40,7 +40,7 @@ usageReport = true)
 */
 @CucumberOptions(features = "src/test/resources/features", 
 glue = { "com.CucumberCraft.stepDefinitions" }, 
-tags = { "@TestId_APS_04"},
+tags = { "@UserStory_US859342"},
 monochrome = true,
 plugin = {
     
