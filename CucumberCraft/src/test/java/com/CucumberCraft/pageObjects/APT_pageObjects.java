7d13fcc2340a44f99eafd057f3f25529d8660750
Ajit <ajit.yadav@aa.com>
2018-11-15 16:58:54 +0530

    update apt objects

--- CucumberCraft/src/test/java/com/CucumberCraft/pageObjects/APT_pageObjects.java
+++ CucumberCraft/src/test/java/com/CucumberCraft/pageObjects/APT_pageObjects.java
@@ -10,7 +10,7 @@ public class APT_pageObjects {
 	private static  String chechkbox="//input[@type='checkbox']";
 	private static  String messages="//input[@id='ptextcode']/parent::div/preceding-sibling::em[.='Required']";
 	private static  String noResponseMessage="h5";
-	private static  String expand ="//img[@id='sp_collapse_icon']";
+	private static  String expand ="//img[@class='img-fluid iconSize']";
    // private static  String expand ="//span[.='Promo code: ']/following-sibling::span/img";
 	private static  String allPromocodeBox="//div[@class='card-header bgtransparent noMarginPad']";
 	private static String allPromocodeNameinHeading="//div//span/b[contains(text(),'Promo code: ')]";
